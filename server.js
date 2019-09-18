4ceb593cded3dbda3f68a237c60829ad1c67d3c3
Your Name <you@example.com>
2019-09-18 11:58:43 +0800

    中间件

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app=express();
 //中间件的使用
 app.use(express.json());
 app.use(express.urlencoded({extend:true}));
-app.use(cookieparse());
+app.use("/hello",cookieparse());
 app.use(express.static("public"));
 app.get("/",(req,res) =>{
     console.log(req.query);
@@ -14,7 +14,7 @@ app.post("/handlelogin",(req,res)=>{
     console.log(req.body);
     res.send("hello express");
 })
-app.get("/setcookie",(req,res)=>{
+app.get("/hello/setcookie",(req,res)=>{
     res.cookie("username","zhangsan",{
         maxAge:1000*60*10,
     });
@@ -25,12 +25,39 @@ app.get("/getcookie",(req,res)=>{
    res.send("cookie获取成功");
 })
 app.get("/hello/:id",(req,res)=>{
-    res.send("我来了");
-    console.log(req.params);
+    res.send();
+    // console.log(req.params);
 })
 app.get("/world/:name/:use",(req,res)=>{
     console.log("hello world");
     console.log(req.params);
     res.end("hello world");
+})//定义一个req.requestTime的方法，实现自定义的中间件
+var myhello=type=>{
+   return (req,res,next)=>{
+       let time=new Date();
+       let year=time.getFullYear();
+       let month=time.getMonth()+1;
+       let date=time.getDate();
+       if(type==1){
+            req.requestTime=year;
+       }else if(type==2){
+            req.requestTime=month;
+       }else if(type==3){
+        req.requestTime=year;
+       }else{
+        req.requestTime=time;
+       }
+        next();
+    };
+}//闭包的方法
+// app.use(myhello(4));//自定义的方法不用加括号,中间件的调用
+app.get("/test",myhello(1),(req,res)=>{//针对某个路由的中间件
+   console.log( req.requestTime);
+   res.send("获取时间");
+})
+app.get("/test1",myhello(2),(req,res)=>{
+    console.log(req.requestTime);
+    res.send("获取时间");
 })
 app.listen(3000);
